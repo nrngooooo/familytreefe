8fbf05c491f04fc282c4d99d56f6b6fe26b1f919
nrngooooo <is21d005@mandakh.edu.mn>
2025-04-21 02:40:33 +0800

    bottom navigation bar style upgraded

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'family_tree_screen.dart';
 import 'api/api_service.dart';
 import 'clan_members_screen.dart';
+import 'package:motion_tab_bar/MotionTabBar.dart';
+import 'package:motion_tab_bar/MotionTabBarController.dart';
 
 class HomeScreen extends StatefulWidget {
   final AuthService authService;
@@ -13,8 +15,26 @@ class HomeScreen extends StatefulWidget {
   _HomeScreenState createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
+class _HomeScreenState extends State<HomeScreen>
+    with SingleTickerProviderStateMixin {
+  MotionTabBarController? _motionTabBarController;
   int _selectedIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _motionTabBarController = MotionTabBarController(
+      initialIndex: 0,
+      length: 3,
+      vsync: this,
+    );
+  }
+
+  @override
+  void dispose() {
+    _motionTabBarController?.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -28,27 +48,29 @@ class _HomeScreenState extends State<HomeScreen> {
                     uid: widget.authService.userInfo?['uid'] ?? '',
                   )
                   : ClanMembersScreen(authService: widget.authService)),
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _selectedIndex,
-        backgroundColor: Colors.grey[300],
-        selectedItemColor: Colors.green,
-        unselectedItemColor: Colors.black,
-        onTap: (index) {
+      bottomNavigationBar: MotionTabBar(
+        controller: _motionTabBarController,
+        initialSelectedTab: "Овгийн гишүүд",
+        labels: const ["Овгийн гишүүд", "Ургийн мод", "Профайл"],
+        icons: const [Icons.family_restroom, Icons.account_tree, Icons.person],
+        tabSize: 50,
+        tabBarHeight: 55,
+        textStyle: const TextStyle(
+          fontSize: 12,
+          color: Colors.black,
+          fontWeight: FontWeight.w500,
+        ),
+        tabIconColor: Colors.black,
+        tabIconSize: 28.0,
+        tabIconSelectedSize: 26.0,
+        tabSelectedColor: Colors.green,
+        tabIconSelectedColor: Colors.white,
+        tabBarColor: Colors.grey[300],
+        onTabItemSelected: (int value) {
           setState(() {
-            _selectedIndex = index;
+            _selectedIndex = value;
           });
         },
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.family_restroom),
-            label: 'Овгийн гишүүд',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.account_tree),
-            label: 'Ургийн мод',
-          ),
-          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Профайл'),
-        ],
       ),
     );
   }
